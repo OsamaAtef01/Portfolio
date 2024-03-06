d9b72bcf44407acfcd030fd364a59ece1579b1e2
Osama Atef <147571519+OsamaAtef01@users.noreply.github.com>
2024-03-06 02:11:08 +0200

    Osama

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/* ----------------To make menu active when cst
+clicks on it--------------------*/
+
+const menu = document.getElementById("menu");
+const action = document.getElementById("actions");
+
+menu.addEventListener("click", () => {
+ handlemenu();
+});
+
+function handlemenu() {
+  
+menu.classList.toggle("is-active");
+action.classList.toggle("is-active");
+};
